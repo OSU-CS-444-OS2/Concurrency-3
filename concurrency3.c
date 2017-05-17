3ec30e8ef4cddf47b48401404fa3b7e204c992e1
Trevor Spear <speart@os-class.engr.oregonstate.edu>
2017-05-16 21:55:45 -0700

    Adding extra stuff

--- concurrency3.c
+++ concurrency3.c
@@ -3,9 +3,14 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <semaphore.h>
+#include <signal.h>
+#include <stdbool.h>
 
 //Global Varibles
-
+pthread_cond_t SearchCondThread;
+pthread_cond_t InsertCondThread;
+pthread_cond_t DeleteCondThread;
+pthread_mutex_t MutexThread;
 
 //Struct
 struct List {
@@ -14,43 +19,72 @@ struct List {
 };
 
 //Functions
+void *Searches();
+void *Inserts();
+void *Deleters();
+int randomNumer();
 
 
 //Main
 int main(){
+	pthread_t SearchThread[3], InsertThread[3], DeleteThread[3];
 
+	for(i = 0; i < 2; i++){
+		pthread_create( &SearchThread[i], NULL, Searches, NULL );
+		pthread_create( &InsertThread[i], NULL, Inserts, NULL );
+		pthread_create( &DeleteThread[i], NULL, Deleters, NULL );
+	}
+
+	for(i = 0; i < 2; i++){
+		pthread_join( SearchThread[i], NULL );
+		pthread_join( InsertThread[i], NULL );
+		pthread_join( DeleteThread[i], NULL );
+	}
 
 	return 0;
 }
 
 //Searchers - merely examine the list; hence they can execute concurrently with each other.
 void *Searches(){
-	//While 1
+	while(1){
+		if(){
 		//Needs to check if there is a lock on search
 			//If not then print Searching through list
-			
-	//End while
+		}		
+	}
 }
 
 //Inserts - add new items to the end of the list; insertions must be mutually exclusive to preclude two inserters from inserting new items at about the same time.
 void *Inserts(){
-	//While 1
+	while(1){
 		//Check if lock
-		//if not then lock it and delete
-			//Check If there is room in the list to add
-				//Add list
-				//unlock
+		if(){
+			//lock it and delete
 
-	//End while
+			//Check If there is room in the list to add
+				//Adds to the end of the list
+				//unlock
+			
+		}
+	}
 }
 
 //Deleters - 
 void *Deleters(){
-	//While 1
+	while(1){
 		//Check if insert is locked if not then lock it and lock search
-			//Delete the last link list
+			//Delete a link
 			//repoint the new list correctly 
 			//unlock
 
-	//End while
+	}
+}
+
+
+//Extra Functions
+int randomNumber(int Min, int Max){
+   	time_t t;
+	srand((unsigned) time(&t));
+
+	return rand() % Max + Min;
 }
