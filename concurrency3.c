df0900f1a6d1a5bc25c6e79fbc82dc5102bc2f56
fankaiyuan <172881969@qq.com>
2017-05-16 23:41:02 -0700

    added id stuff

--- concurrency3.c
+++ concurrency3.c
@@ -28,7 +28,7 @@ int randomNumer();
 
 
 //Main
-int main(){
+int main(int argc, char *argv[]){
 	pthread_t SearchThread[3], InsertThread[3], DeleteThread[3];
     	sem_init( &noSearcher, 0, 3 );
     	sem_init( &noInserter, 0, 1 );
@@ -37,9 +37,10 @@ int main(){
 	int i;
 
 	for( i = 0; i < 2; i++){
-		pthread_create( &SearchThread[i], NULL, Searches, NULL );
-		pthread_create( &InsertThread[i], NULL, Inserts, NULL );
-		pthread_create( &DeleteThread[i], NULL, Deleters, NULL );
+		id[i]=i;
+		pthread_create( &SearchThread[i], NULL, Searches, (void*)(&id[i]) );
+		pthread_create( &InsertThread[i], NULL, Inserts, (void*)(&id[i]) );
+		pthread_create( &DeleteThread[i], NULL, Deleters, (void*)(&id[i]) );
 	}
 
 	for( i = 0; i < 2; i++){
@@ -52,13 +53,14 @@ int main(){
 }
 
 //Searchers - merely examine the list; hence they can execute concurrently with each other.
-void *Searches(){
+void *Searches(void *arg){
+	int searcher_id=*(int *)arg;
 	while(1){
 		sem_wait( &noSearcher );
 		//Needs to check if there is a lock on search
 			//If not then print Searching through list
 
-		printf( "Search Starts\n" );            		
+		printf( "Searcher %d Starts\n",searcher_id );            		
 		struct Link *LittleLink = Head;
 		int i = 1;
 
@@ -66,21 +68,22 @@ void *Searches(){
 			printf( "Searching looking at link %d: %d", i, LittleLink->Number );
 		}
 
-		printf( "Search Ends\n" );            		
+		printf( "Searcher %d Ends\n" ,searcher_id);            		
 		sem_post( &noSearcher );
         	sleep( randomNumber( 2, 5 ) );
 	}
 }
 
 //Inserts - add new items to the end of the list; insertions must be mutually exclusive to preclude two inserters from inserting new items at about the same time.
-void *Inserts(){
+void *Inserts(void *arg){
+	int Inserter_id=*(int *)arg;
 	while(1){
 		if( linkListCount() != 32 ){
 			//Check if lock
 			sem_wait(&noInserter);
 			//pthread_mutex_lock(&insertMutex);
 			//lock it and delete
-			printf( "Insert Starts\n" );            		
+			printf( "Insert %d Starts\n" , Inserter_id);            		
 
 			struct Link *LittleLink = Head, *NewLink;
 
@@ -97,7 +100,7 @@ void *Inserts(){
 			//Adds to the end of the list
         		//pthread_mutex_unlock( &insertMutex );
 			
-			printf( "Insert Ends\n" );            		
+			printf( "Insert %d Ends\n", Inserter_id );            		
 
 			//Unlock
         		sem_post( &noInserter );
@@ -109,7 +112,8 @@ void *Inserts(){
 }
 
 //Deleters -
-void *Deleters(){
+void *Deleters(void *arg){
+	int De_id=*(int *)arg;
 	while(1){
 		if(linkListCount > 1){
 			//Check if insert is locked if not then lock it and lock search
@@ -119,7 +123,7 @@ void *Deleters(){
 			sem_wait( &noSearcher );
 			sem_wait( &noSearcher );
 
-			printf( "Deleter Starts\n" );            		
+			printf( "Deleter %d Starts\n", De_id);            		
 
 			//Delete a link
 			//Repoint the new list correctly
@@ -144,7 +148,7 @@ void *Deleters(){
 			free(DeleteLink);
 			LittleLink->Next = NextLink;
 
-			printf( "Deleter Ends\n" );            		
+			printf( "Deleter %d Ends\n", De_id);            		
 			
 			//Unlock
         		sem_post(&noSearcher);
